69640c78bc7e0147e7e4e644796ab7e1e787be69
root <root@hpanel.hostniki.com>
2026-02-14 01:16:26 +0100

    Include Orion CSS in header.php

--- web/templates/header.php
+++ web/templates/header.php
@@ -5,6 +5,10 @@
 <?php
 require $_SERVER["HESTIA"] . "/web/templates/includes/title.php";
 require $_SERVER["HESTIA"] . "/web/templates/includes/css.php";
+// Include custom Orion Theme CSS
+if (file_exists($_SERVER["HESTIA"] . "/web/css/custom/orion-ui.css")) {
+    echo '<link rel="stylesheet" href="/css/custom/orion-ui.css?v=' . time() . '">';
+}
 require $_SERVER["HESTIA"] . "/web/templates/includes/js.php";
 ?>
 </head>
